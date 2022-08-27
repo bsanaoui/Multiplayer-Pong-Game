6045bba018f4e2179c6327fbcdee5cab09187129
Brahim Sanaoui <bsanaoui@e1r12p11.1337.ma>
2022-08-27 20:00:32 +0100

    handle socket in ChatUI

--- app/src/components/ChatUI.tsx
+++ app/src/components/ChatUI.tsx
@@ -6,7 +6,7 @@ import MessageSent from './MessageSent';
 import MessageRecieved from './MessageRecieved';
 import { useState, useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
-import { TonalitySharp } from '@mui/icons-material';
+import { SafetyCheckSharp, TonalitySharp } from '@mui/icons-material';
 import { connected } from 'process';
 import LoginPage from './LoginPage';
 
@@ -33,24 +33,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
     },
 }));
 
-// const msgs = Array.from({ length: 9 }, (_, index) => {
-//     return (
-//         <div >
-//             <div style={{ float: 'right', marginTop: "5px" }}>
-//                 <MessageSent msg="Caveat with refs Caveat with refs Caveat with refs" />
-//             </div>
-//             <div style={{ float: 'left', marginTop: "5px" }}>
-//                 <MessageRecieved msg="Hello industry. Lorem Ipsum Dom" />
-//             </div>
-//             <div style={{ float: 'left', marginTop: "5px" }}>
-//                 <MessageRecieved msg="Lorem Ipsum Dom ?" />
-//             </div>
-//             <div style={{ float: 'right', marginTop: "5px" }}>
-//                 <MessageSent msg="See you" />
-//             </div>
-//         </div>
-//     );
-// });
+// const msgs = Array.from({ length: 9 }, (_, index) => {return ()}
 const current_user_id: number = 1;  // dynamic handle ????
 
 const initMessages = [
@@ -61,14 +44,18 @@ const initMessages = [
     }
 ];
 
-const renderMessage = (id: number, msg: string): JSX.Element => {
-    if (current_user_id === id)
+const renderMessage = (current: string, usernamee: string, msg: string): JSX.Element => {
+    if (current === usernamee)
         return (
-            <MessageSent msg={msg} />
+            <div style={{ float: 'right', marginTop: "5px" }}>
+                <MessageSent msg={msg} />
+            </div>
         );
     else
         return (
-            <MessageRecieved msg={msg} />
+            <div style={{ float: 'left', marginTop: "5px" }}>
+                <MessageRecieved msg={msg} />
+            </div>
         );
 }
 
@@ -78,27 +65,38 @@ const ChatUI = () => {
     const [connectedUsers, setConnectedUsers] = useState([] as { id: string, username: string }[]);
 
     useEffect(() => {
-        socketclient = io('http://localhost:4000');
+        socketclient = io('http://localhost:3333');
+
         if (socketclient) {
-            socketclient.on("username-taken", () => {
-                console.error("Username is taken") //toast.error
-            });
+            console.log("heeeeeereee");
+            socketclient.on('msgToClient', (messagee: { name: string, text: string }) => {
+                const newMsgs = msgs.concat({ username: messagee.name, id: 1, msg: messagee.text }); // dynamic handle ????
 
-            socketclient.on("username-submitted-successfully", () => {
-                setConnected(true)
-            });
-
-            socketclient.on("get-connected-users", (connectedUsers: { id: string, username: string }[]) => {
-                setConnectedUsers(connectedUsers.filter(user => user.username !== username));
-                console.log(connectedUsers);
+                console.log(msgs);
+                console.log("/------\n-----/");
+                console.log(newMsgs);
+                setMsgs(newMsgs);
             })
         }
+        // if (socketclient) {
+        //     socketclient.on("username-taken", () => {
+        //         console.error("Username is taken") //toast.error
+        //     });
+
+        //     socketclient.on("username-submitted-successfully", () => {
+        //         setConnected(true)
+        //     });
+
+        // socketclient.on("get-connected-users", (connectedUsers: { id: string, username: string }[]) => {
+        //     setConnectedUsers(connectedUsers.filter(user => user.username !== username));
+        //     console.log(connectedUsers);
+        // })
+        // }
     }, [username])
 
     const handleConnection = () => {
-        if (socketclient)
-        {
-            socketclient.emit("handle-connection", username);
+        if (socketclient) {
+            socketclient.emit("JoinRoom1", { name: username, text: '' });
             setConnected(true);
         }
     }
@@ -106,13 +104,15 @@ const ChatUI = () => {
     const [message_input, setMessage] = useState("");
     const [msgs, setMsgs] = useState(initMessages);
 
+
     const handleMsgChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMessage(event.target.value);
     }
     const sendMsg = () => {
         if (message_input) {
-            const newMsgs = msgs.concat({ username: 'bsanaoui', id: 1, msg: message_input }); // dynamic handle ????
+            const newMsgs = msgs.concat({ username: username, id: 1, msg: message_input }); // dynamic handle ????
             setMsgs(newMsgs);
+            socketclient.emit('SendMessageRoom', { name: username, text: message_input });
             console.log("message sent");
         }
     }
@@ -133,34 +133,37 @@ const ChatUI = () => {
                 paddingRight: "20px",
                 borderLeft: "1px solid #FFFFFF"
             }}>
-            {!connected && <LoginPage handleConnection={handleConnection} username={username} setUsername={setUserName}/>}
+            {!connected && <LoginPage handleConnection={handleConnection} username={username} setUsername={setUserName} />}
             {connected && <Stack height='inherit'>
-                    <div>
-                        <HeaderChat name={username} />
-                    </div>
-                    <Stack spacing={2} direction="column-reverse" sx={{ width: "532px", minHeight: "calc( 100vh - 67px )", margin: 'auto' }}>
-                        <Stack direction="row" marginBottom="45px">
-                            <FormControl variant="standard">
-                                <BootstrapInput placeholder="Write a message ..." id="bootstrap-input" onChange={handleMsgChange} onKeyDown={handleEnterkey} />
-                            </FormControl>
-                            <div style={{
-                                backgroundColor: "#151416", padding: "10px", borderRadius: '0 12px 12px 0',
-                            }}>
-                                <Button sx={{ backgroundColor: "#3475D7", height: "50px", color: "#FFF" }} onClick={sendMsg}>
-                                    <SendIcon />
-                                </Button>
-                            </div>
-                        </Stack>
-                        <List style={{ overflow: 'auto', padding: '0 6px 0px 5px' }} >
-                            {/* {msgs} */}
-                            {msgs.map((item) => (
-                                <div style={{ float: 'right', marginTop: "5px" }}>
-                                    {renderMessage(item.id, item.msg)}
-                                </div>
-                            ))}
-                        </List>
+                <div>
+                    <HeaderChat name={username} />
+                </div>
+                <Stack spacing={2} direction="column-reverse" sx={{ width: "532px", minHeight: "calc( 100vh - 67px )", margin: 'auto' }}>
+                    <Stack direction="row" marginBottom="45px">
+                        <FormControl variant="standard">
+                            <BootstrapInput placeholder="Write a message ..." id="bootstrap-input" onChange={handleMsgChange} onKeyDown={handleEnterkey} />
+                        </FormControl>
+                        <div style={{
+                            backgroundColor: "#151416", padding: "10px", borderRadius: '0 12px 12px 0',
+                        }}>
+                            <Button sx={{ backgroundColor: "#3475D7", height: "50px", color: "#FFF" }} onClick={sendMsg}>
+                                <SendIcon />
+                            </Button>
+                        </div>
                     </Stack>
+                    <List style={{ overflow: 'auto', padding: '0 6px 0px 5px' }} >
+                        {/* {msgs} */}
+                        <div>
+
+                        {msgs.map((item) => (
+                            <div style={{ float: 'right', marginTop: "5px" }}>
+                                {renderMessage(username, item.username, item.msg)}
+                            </div>
+                        ))}
+                        </div>
+                    </List>
                 </Stack>
+            </Stack>
             }
         </Box>
     )
