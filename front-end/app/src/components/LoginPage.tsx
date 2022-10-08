12f7c45e9ff841d0a0cb7650385fd1f3e9f53fe1
Brahim Sanaoui <bsanaoui@e1r11p12.1337.ma>
2022-10-08 01:37:58 +0100

    Fix Login

--- front-end/app/src/components/LoginPage.tsx
+++ front-end/app/src/components/LoginPage.tsx
@@ -14,39 +14,62 @@ const LoginPage = () => {
 			sx={{
 				height: '100%',
 				width: '100%',
-				position:"relative",
-				// background: "linear-gradient( 116.27deg, #191D45 31.5%, #4044A5 61.17% )",
-				// "::before": {
-				// 	// content: "",
+			}}>
+			<Box
+				sx={{
 					backgroundImage: `url(${backgroundgif})`,
 					backgroundRepeat: 'no-repeat',
 					backgroundSize: 'cover',
-					// position: 'absolute',
-					// width: "100%",
-					// height: "100%",
+					position: 'absolute',
+					width: "100%",
+					height: "100%",
 					left: 0,
 					top: 0,
-					// boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-					// opacity: 0.8,
-				// }
-			}}
-		>
-			<Stack
-				className="home__container"
-				margin="auto"
-				width="50vw"
-				height="100vh"
-				direction="column"
-				justifyContent="center"
-				alignItems="center"
-				spacing={2}>
-				<img src={ImagePong}></img>
-				<Typography className="hometitle" sx={{
-					width: "80%",
-					fontWeight: '700',
-					fontSize: '2.9vw',
-					textAlign: 'center',
-				}}>
+					filter: 'blur(3px)',
+					opacity: "0.8",
+					// }
+				}}
+			>
+			</Box>
+			<Box
+				sx={{
+					height: '100%',
+					width: '100%',
+					position: "relative",
+					// background: "linear-gradient( 116.27deg, #191D45 31.5%, #4044A5 61.17% )",
+					// "&::before": {
+					// 	// content: "",
+					// 	backgroundImage: `url(${backgroundgif})`,
+					// 	backgroundRepeat: 'no-repeat',
+					// 	backgroundSize: 'cover',
+					// 	position: 'absolute',
+					// 	width: "100%",
+					// 	height: "100%",
+					// 	left: 0,
+					// 	top: 0,
+					// 	// boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+					// 	// opacity: 0.8,
+					// 	filter: 'blur(8px)',
+					// }
+				}}
+			>
+				<Stack
+					className="home__container"
+					margin="auto"
+					width="50vw"
+					height="100vh"
+					direction="column"
+					justifyContent="center"
+					alignItems="center"
+					spacing={2}>
+					<img src={ImagePong}></img>
+					<Typography className="hometitle" sx={{
+						width: "80%",
+						fontWeight: '700',
+						fontSize: '2.9vw',
+						textAlign: 'center',
+						textShadow: "2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000, 1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000",
+					}}>
 					Playing games together
 					has never been so easy
 				</Typography>
@@ -68,6 +91,7 @@ const LoginPage = () => {
 				</form>
 			</Stack >
 		</Box>
+		</Box >
 	)
 }
 
